62da5d698a796138ae1e76bfd2f3762092358713
wuwutao <2382328231@qq.com>
2021-03-29 19:43:48 +0800

    数组和元组

--- demo02.py
+++ demo02.py
@@ -1,4 +1,44 @@
 
-a = input("输入你的内容:")
+"""
+print(a[2])
 
-print("a")
+print(a.count(3))
+print(a.index(3))
+
+b=(a,"haha","hihi",3)
+
+print(b[0][1])
+
+
+a=("sss","ss",1,2,3,3,3,3,3,"哈哈","嘻嘻")
+
+#切片
+print(a[:])
+print(a[:2])
+print(a[2:9])
+print(a[9:])
+
+"""
+a=["sss","ss",1,2,3,3,3,3,3,"哈哈","嘻嘻"]
+a.append(5)
+print(a)
+
+a.insert(5,"wwt")
+print(a)
+
+a.pop(5)
+print(a)
+
+#数组进行切片
+print(a[0:2])
+print(a[2:9])
+
+x=["a","b","c"]
+a.extend(x)  
+print(a)
+
+y=["as","ss"]
+print(a+y)
+
+print(a)
+
